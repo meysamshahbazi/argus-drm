4bdda0e03c1806c5ddd8026d32270fc3cc7200a7
meysam <mysm.shahbazi@gmail.com>
2025-08-13 08:50:18 +0330

    Add cmake

---
+++ main.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <opencv2/core.hpp>
+
+#include <gst/gst.h>
+
+
+
+
+int main(int argc, char** argv) {
+    // saveEngineFile("/home/user/data/rdian.onnx","/home/user/data/rdian.engine");
+    // stmtracker::gen_engine_from_onnx();
+    // return -1;
+    
+    // gst_init(&argc, &argv);
+
+    return 0;
+}
